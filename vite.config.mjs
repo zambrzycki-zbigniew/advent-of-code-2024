c92f2877ceda70aeaaf1f02010d2b1f1f0e12f95
Zbigniew Zambrzycki <spamherenoobs@gmail.com>
2024-12-02 03:44:38 +0100

    fetching leaderboard

--- vite.config.mjs
+++ vite.config.mjs
@@ -15,81 +15,85 @@ import { fileURLToPath, URL } from 'node:url'
 console.log('Alias @ resolves to:', fileURLToPath(new URL('./src', import.meta.url)));
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/advent-of-code-2024/',
-  build: {
-    outDir: 'build',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name].[hash].js',
-        chunkFileNames: 'assets/[name].[hash].js',
-        assetFileNames: 'assets/[name].[hash].[ext]',
-        format: 'es',
+export default defineConfig(({ mode }) => {
+  console.log(mode)
+  const base = mode === 'production' ? '/advent-of-code-2024/' : '/';
+  return {
+    base,
+    build: {
+      outDir: 'build',
+      assetsDir: 'assets',
+      rollupOptions: {
+        output: {
+          entryFileNames: 'assets/[name].[hash].js',
+          chunkFileNames: 'assets/[name].[hash].js',
+          assetFileNames: 'assets/[name].[hash].[ext]',
+          format: 'es',
+        },
       },
     },
-  },
-  worker: {
-    format: 'es',
-  },
-  plugins: [
-    VueRouter(),
-    Layouts(),
-    Vue({
-      template: { transformAssetUrls }
-    }),
-    // https://github.com/vuetifyjs/vuetify-loader/tree/master/packages/vite-plugin#readme
-    Vuetify({
-      autoImport: true,
-      styles: {
-        configFile: 'src/styles/settings.scss',
-      },
-    }),
-    Components(),
-    Fonts({
-      google: {
-        families: [{
-          name: 'Roboto',
-          styles: 'wght@100;300;400;500;700;900',
-          display: 'swap',
-        }],
-      },
-    }),
-    AutoImport({
-      imports: [
-        'vue',
-        'vue-router',
-      ],
-      eslintrc: {
-        enabled: true,
-      },
-      vueTemplate: true,
-    }),
-  ],
-  define: { 'process.env': {} },
-  resolve: {
-    alias: [{
-      find: '@',
-      replacement: fileURLToPath(new URL('./src', import.meta.url))
-    }],
-    extensions: [
-      '.js',
-      '.json',
-      '.jsx',
-      '.mjs',
-      '.ts',
-      '.tsx',
-      '.vue',
+    worker: {
+      format: 'es',
+    },
+    plugins: [
+      VueRouter(),
+      Layouts(),
+      Vue({
+        template: { transformAssetUrls }
+      }),
+      // https://github.com/vuetifyjs/vuetify-loader/tree/master/packages/vite-plugin#readme
+      Vuetify({
+        autoImport: true,
+        styles: {
+          configFile: 'src/styles/settings.scss',
+        },
+      }),
+      Components(),
+      Fonts({
+        google: {
+          families: [{
+            name: 'Roboto',
+            styles: 'wght@100;300;400;500;700;900',
+            display: 'swap',
+          }],
+        },
+      }),
+      AutoImport({
+        imports: [
+          'vue',
+          'vue-router',
+        ],
+        eslintrc: {
+          enabled: true,
+        },
+        vueTemplate: true,
+      }),
     ],
-  },
-  server: {
-    port: 3000,
-  },
-  css: {
-    preprocessorOptions: {
-      sass: {
-        api: 'modern-compiler',
+    define: { 'process.env': {} },
+    resolve: {
+      alias: [{
+        find: '@',
+        replacement: fileURLToPath(new URL('./src', import.meta.url))
+      }],
+      extensions: [
+        '.js',
+        '.json',
+        '.jsx',
+        '.mjs',
+        '.ts',
+        '.tsx',
+        '.vue',
+      ],
+    },
+    server: {
+      port: 3000,
+    },
+    css: {
+      preprocessorOptions: {
+        sass: {
+          api: 'modern-compiler',
+        },
       },
     },
-  },
+  };
 })
